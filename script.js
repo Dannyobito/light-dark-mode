803b55b86c4caf201682216d5f8c969096b7de71
Dannyobito <dannyobito05@gmail.com>
2023-08-28 13:34:31 +0100

    enabled saving theme data to local storage

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+//Dark or light image theme
+const imageMode = (color) => {
+    image1.src = `img/undraw_proud_coder_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`
+}
+
 //Dark Mode Styling
 const darkMode = () => {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
@@ -13,9 +20,7 @@ const darkMode = () => {
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_proud_coder_dark.svg'
-    image2.src = 'img/undraw_feeling_proud_dark.svg'
-    image3.src = 'img/undraw_conceptual_idea_dark.svg'
+    imageMode('dark');
 }
 const lightMode = () => {
     nav.style.backgroundColor = 'rgb(255 255 255 / 70%)';
@@ -23,21 +28,32 @@ const lightMode = () => {
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.remove('fa-moon');
     toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg'
-    image2.src = 'img/undraw_feeling_proud_light.svg'
-    image3.src = 'img/undraw_conceptual_idea_light.svg'
+    imageMode('light');
 }
 
 //Switch Theme
 const switchTheme = (event) =>{
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme','dark');
+        localStorage.setItem('theme','dark');
         darkMode();
     }else {
         document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme','light');
         lightMode();
     }
 }
 
 //Event Listener
 toggleSwitch.addEventListener('change',switchTheme)
+
+//Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
